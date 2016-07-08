7fa99e18e7728db3cc2849446320c9f42ada38f7
Raghu Betina <rbetina@gmail.com>
2016-07-08 07:24:44 -0500

    Add XML config

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,15 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def xml_config
+    respond_to do |format|
+      format.xml do
+        tc = IMS::LTI::ToolConfig.new(:title => "Rails LTI 1.1 Tool Provider Example", :launch_url => launch_url)
+        tc.description = "This example LTI Tool Provider supports LIS Outcome pass-back."
+
+        render :xml => tc.to_xml
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
+
+  get "config" => "pages#xml_config"
+
+  post "launch" => "pages#launch", as: :launch
 end
